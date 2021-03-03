776fea7b98e7eb622d4390e21d831d8dd8593a62
Sami Juvonen <sami.juvonen@serverit.fi>
2021-03-03 19:36:54 +0200

    Client: Added React Query -library

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css';
 import App from './App';
 import store from './store';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
